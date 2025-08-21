d4289d7417f668a8ccbe3046582b729ca0e10855
Hiromichi-Akiyama <fox37450@gmail.com>
2025-08-21 21:21:57 +0900

    微修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,13 +3,9 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :token
-    validates :city
-    validates :address
-
-    # フォーマット/値のチェックと存在チェックを同時に行う
+    # 存在チェック
+    validates :user_id, :item_id, :token, :city, :address
+    # 存在チェックとフォーマット/値のチェック
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
